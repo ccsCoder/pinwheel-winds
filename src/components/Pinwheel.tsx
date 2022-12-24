89d643a99adf9fa77f1db7521883e0cd6a1ca260
Nishant Gupta <ccsCoder@gmail.com>
2022-12-24 13:00:31 +0000

    Working Dynamic Rotation Animation

--- src/components/Pinwheel.tsx
+++ src/components/Pinwheel.tsx
@@ -1,14 +1,12 @@
-import React, { RefObject, useEffect, useRef } from 'react';
-import styled, { keyframes } from 'styled-components';
+import React, { useEffect, useRef } from 'react';
+import styled, { createGlobalStyle, keyframes } from 'styled-components';
 
 // calculates the timing of the animation w.r.t tangential speed.
-const computeRotationTiming = (windSpeed: number, wheelRef: RefObject<HTMLImageElement>): number => {
+const computeRotationTiming = (windSpeed: number, width: number): number => {
+  // console.log('##wheelRef =', wheelRef)
   if (windSpeed === 0) return Number.POSITIVE_INFINITY
-  if (wheelRef.current === null) return Number.POSITIVE_INFINITY
-  // we have finite wind speed otherwise
-  const radius = wheelRef.current.getBoundingClientRect().height
   // tangential velocity timing function is 2 Pi R/ windSpeed
-  return (2 * Math.PI * radius) / windSpeed
+  return (2 * Math.PI * width) / windSpeed
 }
 
 const rotate = keyframes`
@@ -21,20 +19,29 @@ const rotate = keyframes`
   }
 `;
 
-const WheelImg = styled.img.attrs((props: {windSpeed: number, ref: RefObject<HTMLImageElement>}) => props)`
+
+const WheelImg = styled.img`
   height: 40%;
   aspect-ratio: 1;
   transform-origin: center;
-  animation: ${rotate} ${props => computeRotationTiming(props.windSpeed, props.ref)}s linear infinite;
+  animation-name: ${rotate};
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
 `
 
 
 const Pinwheel = ({ windSpeed = 0 }: { windSpeed : number}) => {
     const wheelRef = useRef<HTMLImageElement>(null)
-    
-    return <>
-      <WheelImg ref={wheelRef} windSpeed={windSpeed} src={require("./pinwheel.png")} alt="A pinwheel that moves according to local wind conditions" />
-    </>;
+    useEffect(() => {
+      const current = wheelRef.current!;
+      const animationSpeed = computeRotationTiming(windSpeed, current.getBoundingClientRect().width as number);
+      console.log('Animation Speed = ', animationSpeed)
+      current.style.animationDuration = `${animationSpeed / 1000.0}s`;
+      return () => {
+        current.style.animationDuration = `${Number.POSITIVE_INFINITY}s`;
+      }
+    }, [])
+    return <WheelImg ref={wheelRef} src={require("./pinwheel.png")} alt="A pinwheel that moves according to local wind conditions" />  
 }
  
 export default Pinwheel;
