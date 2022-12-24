2e700becc73fe3d18bc5879c6881f6492007ec52
Nishant Gupta <ccsCoder@gmail.com>
2022-12-24 13:10:29 +0000

    Fetching location coordinates. Will pass down app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Pinwheel from './components/Pinwheel';
 import styled from 'styled-components'
@@ -19,10 +19,34 @@ const AppMain = styled.main`
 `
 
 const App = () => {
+  useEffect(() => {
+    // request browser location here...
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    
+    const success = (pos: GeolocationPosition) => {
+      const crd = pos.coords;
+    
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+    
+    const error = (err: any) => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+      alert('Unable to fetch your location. Please allow from Settings or the website prompt.');
+    }
+    
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, [])
   return (
     <>
       <AppMain> 
-        <Pinwheel windSpeed={1}/>
+        <Pinwheel windSpeed={0}/>
       </AppMain>
     </>
     
